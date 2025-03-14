0dae0bdf5196c9cd833b16f695b82cf8e17c9f27
Elsayed_ayman <eelsayedayman@gmail.com>
2025-03-14 02:44:13 +0200

    Update main.py

--- parts_scraper/main.py
+++ parts_scraper/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
 
@@ -75,4 +76,5 @@ def search():
     return jsonify(all_results)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
